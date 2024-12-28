796f5d1527cd2651136c9da0e4755411e0711e36
Shiva Krishna D <shiva@xcontrol.de>
2024-12-28 11:04:54 +0100

    Add fonts and theming

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { auth, signOut, signIn } from "@/auth";
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = async () => {
   const session = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
-        <Link href="/">logo</Link>
+        <Link href="/">
+          <Image src="/logo.png" alt="YC Directory" width={144} height={30} />
+        </Link>
         <div className="flex items-center gap-5 text-black">
           {session && session.user ? (
             <>
