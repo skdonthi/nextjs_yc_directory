307fe78992b8e0918cbcfb746b657d10f25b4c0a
Shiva Krishna D <shiva@xcontrol.de>
2025-01-07 16:36:29 +0100

    Add route/serach params and search, x icons

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -12,7 +17,7 @@ export default function Home() {
           Competetions.
         </p>
 
-        <SearchForm />
+        <SearchForm query={query} />
       </section>
     </>
   );
--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { X } from "lucide-react";
 import Link from "next/link";
 
 const SearchFormReset = () => {
@@ -13,7 +14,7 @@ const SearchFormReset = () => {
     <>
       <button type="submit" onClick={reset}>
         <Link href="/" className="search-btn text-white">
-          X
+          <X className="size-5" />
         </Link>
       </button>
     </>
