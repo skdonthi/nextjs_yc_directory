6d34db8df3f96eb32d260e770365507fed6cc6e6
Shiva Krishna D <shiva@xcontrol.de>
2025-01-13 15:53:01 +0100

    add search section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,6 +19,12 @@ export default async function Home({
 
         <SearchForm query={query} />
       </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+      </section>
     </>
   );
 }
